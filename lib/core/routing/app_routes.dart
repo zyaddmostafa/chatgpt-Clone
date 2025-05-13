2703c7e2f34f2a2e8d2ccbb48f56a29e612440ee
zyad mostafa <zyadmo026@gmail.com>
2025-05-13 10:32:16 +0200

    Add routing and extension utilities for navigation and data handling

---
+++ lib/core/routing/app_routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class AppRoutes {
+  Route? generateRoute(RouteSettings settings) {
+    // final args = settings.arguments;
+
+    switch (settings.name) {
+      default:
+        return null;
+    }
+  }
+}
